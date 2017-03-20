2248ee5ffc89aa191df4f52b153c23f669a4e87c
Kirill Kostikov <6Crocuta9@gmail.com>
2017-03-20 05:05:24 +0300

    Сделал адаптивный footer

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
+var pageHeader = document.querySelector('.page-header');
 var navToggle = document.querySelector('.main-nav__toggle');
 var navMain = document.querySelector('.main-nav');
 var modalWindow = document.querySelector('.modal-add-to-cart');
 var btnAddInCart = document.querySelector('.btn--add-in-cart');
 var navMainList = document.querySelector('.main-nav__list--nojs');
 
+pageHeader.classList.remove('page-header--nojs');
+navToggle.classList.remove('main-nav__toggle--nojs');
 navMain.classList.remove('main-nav--nojs');
 navMainList.classList.remove('main-nav__list--nojs');
 
@@ -13,9 +16,11 @@ navToggle.addEventListener('click', function(event){
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    navToggle.classList.add('main-nav__toggle--opened');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    navToggle.classList.remove('main-nav__toggle--opened');
   }
 });
 
