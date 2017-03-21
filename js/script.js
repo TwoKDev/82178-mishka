1294c1818e53e2bc53895d1644e3ff9dbfb8ae4b
Kirill Kostikov <6Crocuta9@gmail.com>
2017-03-22 01:34:46 +0300

    Сделал модальное окно

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-var pageHeader = document.querySelector('.page-header');
+var pageHeader = document.querySelector('.page-header__inner');
 var navToggle = document.querySelector('.main-nav__toggle');
 var navMain = document.querySelector('.main-nav');
 var modalWindow = document.querySelector('.modal-add-to-cart');
-var btnAddInCart = document.querySelector('.btn--add-in-cart');
+var modalOverlay = document.querySelector('.modal-overlay');
+var btnOpenModal = document.querySelector('.btn--open-modal-window');
 var navMainList = document.querySelector('.main-nav__list--nojs');
 
-pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove('page-header__inner--nojs');
 navToggle.classList.remove('main-nav__toggle--nojs');
 navMain.classList.remove('main-nav--nojs');
 navMainList.classList.remove('main-nav__list--nojs');
@@ -24,6 +25,21 @@ navToggle.addEventListener('click', function(event){
   }
 });
 
-btnAddInCart.addEventListener('click', function(event) {
+btnOpenModal.addEventListener('click', function(event) {
   event.preventDefault();
+
+  if (modalWindow.classList.contains('modal-add-to-cart--closed')) {
+    modalWindow.classList.remove('modal-add-to-cart--closed');
+    modalWindow.classList.add('modal-add-to-cart--opened');
+    modalOverlay.classList.add('modal-overlay--opened');
+  }
+});
+
+modalOverlay.addEventListener('click', function(event) {
+  if (modalWindow.classList.contains('modal-add-to-cart--opened')) {
+    modalWindow.classList.add('modal-add-to-cart--closed');
+    modalOverlay.classList.add('modal-overlay--closed');
+    modalWindow.classList.remove('modal-add-to-cart--opened');
+    modalOverlay.classList.remove('modal-overlay--opened');
+  }
 });
