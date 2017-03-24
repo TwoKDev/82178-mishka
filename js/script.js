a7143af5f1e70903663f898db28e9a78f05191f2
Kirill Kostikov <6Crocuta9@gmail.com>
2017-03-24 18:55:31 +0300

    Сделал эффекты на элементах, кроме двух кнопок на на главной

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var navToggle = document.querySelector(".main-nav__toggle");
 var navMain = document.querySelector(".main-nav");
 var modalWindow = document.querySelector(".modal-add-to-cart");
 var modalOverlay = document.querySelector(".modal-overlay");
-var btnOpenModal = document.querySelector(".btn--open-modal-window");
+var btnOpenModal = document.querySelectorAll(".btn--open-modal-window");
 var navMainList = document.querySelector(".main-nav__list--nojs");
 
 pageHeader.classList.remove("page-header__inner--nojs");
@@ -25,14 +25,16 @@ navToggle.addEventListener("click", function(event){
   }
 });
 
-btnOpenModal.addEventListener("click", function(event) {
-  event.preventDefault();
+Array.prototype.forEach.call(btnOpenModal, function(button) {
+  button.addEventListener("click", function(event) {
+    event.preventDefault();
 
-  if (modalWindow.classList.contains("modal-add-to-cart--closed")) {
-    modalWindow.classList.remove("modal-add-to-cart--closed");
-    modalWindow.classList.add("modal-add-to-cart--opened");
-    modalOverlay.classList.add("modal-overlay--opened");
-  }
+    if (modalWindow.classList.contains("modal-add-to-cart--closed")) {
+      modalWindow.classList.remove("modal-add-to-cart--closed");
+      modalWindow.classList.add("modal-add-to-cart--opened");
+      modalOverlay.classList.add("modal-overlay--opened");
+    }
+  });
 });
 
 modalOverlay.addEventListener("click", function(event) {
