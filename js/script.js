93d1372f866a7e7f250412445b06b64dd60b7736
Kirill Kostikov <6Crocuta9@gmail.com>
2017-03-31 00:59:27 +0300

    Переделал меню сделал пока планшетную и десктопную версию

--- js/script.js
+++ js/script.js
@@ -1,27 +1,50 @@
 var pageHeader = document.querySelector('.page-header__inner');
-var navToggle = document.querySelector('.page-header__toggle');
-var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav__list');
 var modalWindow = document.querySelector('.modal-add-to-cart');
 var modalOverlay = document.querySelector('.modal-overlay');
 var btnOpenModal = document.querySelectorAll('.btn--open-modal-window');
-var navMainList = document.querySelector('.main-nav__list--nojs');
 
 pageHeader.classList.remove('page-header__inner--nojs');
-navToggle.classList.remove('page-header__toggle--nojs');
-navMain.classList.remove('main-nav--nojs');
-navMainList.classList.remove('main-nav__list--nojs');
+navToggle.classList.remove('main-nav__toggle--nojs');
+navMain.classList.remove('main-nav__list--nojs');
 
-navToggle.addEventListener('click', function(event){
-  event.preventDefault();
+// navToggle.addEventListener('click', function(event){
+//   event.preventDefault();
+//
+//   if (navMain.classList.contains('main-nav__list--closed')) {
+//     navMain.classList.remove('main-nav__list--closed');
+//     navMain.classList.add('main-nav__list--opened');
+//     navToggle.classList.add('main-nav__toggle--opened');
+//   } else {
+//     navMain.classList.add('main-nav__list--closed');
+//     navMain.classList.remove('main-nav__list--opened');
+//     navToggle.classList.remove('main-nav__toggle--opened');
+//   }
+// });
 
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navToggle.classList.add('page-header__toggle--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navToggle.classList.remove('page-header__toggle--opened');
+Array.prototype.forEach.call(navToggle, function(button) {
+  button.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (navMain.classList.contains('main-nav__list--closed')) {
+      navMain.classList.remove('main-nav__list--closed');
+      navMain.classList.add('main-nav__list--opened');
+      navToggle.classList.add('main-nav__toggle--opened');
+    } else {
+      navMain.classList.add('main-nav__list--closed');
+      navMain.classList.remove('main-nav__list--opened');
+      navToggle.classList.remove('main-nav__toggle--opened');
+    }
+  });
+});
+
+modalOverlay.addEventListener('click', function(event) {
+  if (modalWindow.classList.contains('modal-add-to-cart--opened')) {
+    modalWindow.classList.add('modal-add-to-cart--closed');
+    modalOverlay.classList.add('modal-overlay--closed');
+    modalWindow.classList.remove('modal-add-to-cart--opened');
+    modalOverlay.classList.remove('modal-overlay--opened');
   }
 });
 
